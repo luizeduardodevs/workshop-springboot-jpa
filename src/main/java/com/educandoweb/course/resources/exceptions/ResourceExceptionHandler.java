0ec04830cbb862ce1b221aa4e499d705de50c14f
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-06-16 00:17:19 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,5 +23,11 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 		
 	}
+	@ExceptionHandler(DataBaseException.class)//o nome da exceção que será interceptada, esse metodo vai interceptar qualquer exceção do tipo que esta entre () 
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error,e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
 
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 //@Component anotação pra registrar como componente, pra fazer a injenção, se tornar um componente do spring
@@ -32,14 +37,23 @@ public class UserService {
 		return repository.save(obj); //save ja retorna o objeto salvo
 	}
 	public void delete (Long id) {
+		try {//lançado a minha exceção de serviço pra ids nao existentes 
 		repository.deleteById(id);//deletando
+	}catch(EmptyResultDataAccessException e) {	
+		throw new ResourceNotFoundException(id);
+	}catch(DataIntegrityViolationException e) {
+		throw new DataBaseException(e.getMessage());//lançado exceção da minha camada de serviço
 	}
-	
+	}
 	public User update(Long id, User obj){// retorna o usuario, qual o usuario que vai retorna o id, e quais os dados sera atualizados do USer OBJ
+	try {//trtadno execeção para se caso for atualizar algum usuario sem id existente
 	User entity = repository.getReferenceById(id);//  getreference ele vaio instacis o usuário, porem não vai no banco de dados , só deixa ele monitorado pelo JPA
 	// atualizando o entity com os dados que vieram dentro do OBJ
 	updateData(entity,obj);
 	return repository.save(entity);
+	}catch(EntityNotFoundException e) {
+		throw new ResourceNotFoundException(id);		
+	}
 }
 
 	private void updateData(User entity, User obj) {
